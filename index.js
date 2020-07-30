ea27ed8707a0bbb5d2c9797e8712aee122ac0813
Himanshu Sisodiya <6894himsy@gmail.com>
2020-07-30 12:23:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+class Node {
+  constructor(value){
+    this.value = value,
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(value){
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value){
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value){
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  insert(index, value){
+    if (index == 0) {
+      this.prepend(value);
+    } else {
+      const newNode = new Node(value);
+      var temp = this.head;
+      for(let i=0; i<index-1; i++){
+        temp = temp.next;
+      }
+      console.log(temp.value);
+
+      newNode.next = temp.next;
+      temp.next = newNode;
+      this.length++;
+    }
+  }
+
+  printList(){
+    let array = [];
+    let currentNode = this.head;
+    while(currentNode != null){
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+  }
+
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(20);
+myLinkedList.append(30);
+myLinkedList.prepend(5);
+myLinkedList.insert(1, 15);
+myLinkedList.printList();
